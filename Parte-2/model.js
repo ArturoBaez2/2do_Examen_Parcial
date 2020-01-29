f187460dc7adfd02088ac6da81055f172adf060c
Achuk777 <akbaez@hotmail.com>
2020-01-29 08:49:44 -0600

    Second commit

--- Parte-2/model.js
+++ Parte-2/model.js
@@ -4,6 +4,12 @@ let uuid = require( 'uuid' );
 mongoose.Promise = global.Promise;
 
 /* Tu código va aquí */
+let bookmarksDB = {
+	id : { type : uuid.v4()},
+	titulo : {type : String },
+	descripcion : { type : String },
+	url : { type : String }
+}
 
 module.exports = {
     
